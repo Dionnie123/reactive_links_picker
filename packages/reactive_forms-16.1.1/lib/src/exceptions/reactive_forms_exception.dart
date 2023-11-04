1037dab37b63a423a2b27d8cc69a79b1cbd0f123
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2023-11-04 17:42:57 +0800

    up

--- packages/reactive_forms-16.1.1/lib/src/exceptions/reactive_forms_exception.dart
+++
@@ -1,2 +0,0 @@
-/// Represents the base class for all the Reactive Forms Exceptions
-abstract class ReactiveFormsException implements Exception {}
--- packages/reactive_forms-16.1.1/lib/src/validators/pattern/pattern_evaluator.dart
+++
@@ -1,8 +0,0 @@
-/// Represents a class that evaluates a pattern
-abstract class PatternEvaluator {
-  /// Returns the string representation of the pattern
-  String get pattern;
-
-  /// Evaluates if the the patter has a match with the [input]
-  bool hasMatch(String input);
-}
